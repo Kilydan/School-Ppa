271acce3e9789d68cc517c3a025575e8fba4214d
Kilydan <tim.hoenselaar@gmail.com>
2015-10-13 13:01:14 +0200

    fdsafdsafdsa

--- app/src/main/java/com/internet/internet/MainActivity.java
+++ app/src/main/java/com/internet/internet/MainActivity.java
@@ -1,12 +1,8 @@
 package com.internet.internet;
 
 import android.app.Activity;
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.os.Bundle;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -19,30 +15,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         final WebView webview = new WebView(this);
         setContentView(webview);
-
         webview.getSettings().setJavaScriptEnabled(true);
         final Activity activity = this;
-        webview.setWebChromeClient(new WebChromeClient() {;
-        public void onProgressChanged(WebView View, int progress){
-            Toast.makeText(activity, Integer.toString(progress) + "%",
-                    Toast.LENGTH_SHORT).show();
-        }
-            });
-
-        webview.setWebViewClient(new WebViewClient() {
-            public void onReceivedError(WebView view, int errorcode, String
-                    description, String failingUrl) {
+        webview.setWebChromeClient(new WebChromeClient() {
+            public void onProgressChanged(WebView view, int progress) {
+                Toast.makeText(activity, Integer.toString(progress) + "%", Toast.LENGTH_SHORT).show();
+            }
+        });
+        webview.setWebViewClient(new WebViewClient(){
+            public void onReceivedError(WebView view, int errorCode, String description, String failingURL) {
                 Toast.makeText(activity, "Error!" + description, Toast.LENGTH_SHORT).show();
             }
-
-            public void onPageFinished(WebView view, String url) {
+            public void onPageFinished(WebView view, String url){
                 Toast.makeText(activity, url, Toast.LENGTH_SHORT).show();
             }
-
         });
-                webview.loadUrl("http://hugelol.com/");
-
+        webview.loadUrl("https://www.google.nl/maps/@51.5412025,5.2672179,9z?hl=en");
     }
-
-
 }
