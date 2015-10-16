8eb4386dd52fd99f013ba5497e3e4572eeab15b0
Kilydan <tim.hoenselaar@gmail.com>
2015-10-16 12:25:05 +0200

    dfsgfds

--- app/src/main/java/com/internet/internet/MainActivity.java
+++ app/src/main/java/com/internet/internet/MainActivity.java
@@ -1,6 +1,7 @@
 package com.internet.internet;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -17,28 +18,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_main);
-
-
-
-
-        final WebView webview = new WebView(this);
-        setContentView(webview);
-        webview.getSettings().setJavaScriptEnabled(true);
-        //kek
-        final Activity activity = this;
-        webview.setWebChromeClient(new WebChromeClient() {
-            public void onProgressChanged(WebView view, int progress) {
-                Toast.makeText(activity, Integer.toString(progress) + "%", Toast.LENGTH_SHORT).show();
-            }
-        });
-        webview.setWebViewClient(new WebViewClient(){
-            public void onReceivedError(WebView view, int errorCode, String description, String failingURL) {
-                Toast.makeText(activity, "Error!" + description, Toast.LENGTH_SHORT).show();
-            }
-            public void onPageFinished(WebView view, String url){
-                Toast.makeText(activity, url, Toast.LENGTH_SHORT).show();
-            }
-        });
-        webview.loadUrl("https://www.google.nl/maps/place/Nederland/@52.191735,3.0369282,7z/data=!3m1!4b1!4m2!3m1!1s0x47c609c3db87e4bb:0xb3a175ceffbd0a9f");
     }
+
+    public void onClickMaps(View view) {
+        Intent maps = new Intent(Intent.ACTION_VIEW);
+        startActivity(maps);
+    }
+
 }
--- app/src/main/java/com/internet/internet/Maps.java
+++ app/src/main/java/com/internet/internet/Maps.java
@@ -1,11 +1,16 @@
 package com.internet.internet;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class Maps extends AppCompatActivity {
 
@@ -13,6 +18,7 @@ public class Maps extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -24,6 +30,27 @@ public class Maps extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        final WebView webview = new WebView(this);
+        setContentView(webview);
+        webview.getSettings().setJavaScriptEnabled(true);
+        final Activity activity = this;
+        webview.setWebChromeClient(new WebChromeClient() {
+            public void onProgressChanged(WebView view, int progress) {
+                Toast.makeText(activity, Integer.toString(progress) + "%", Toast.LENGTH_SHORT).show();
+            }
+        });
+        webview.setWebViewClient(new WebViewClient() {
+            public void onReceivedError(WebView view, int errorCode, String description, String failingURL) {
+                Toast.makeText(activity, "Error!" + description, Toast.LENGTH_SHORT).show();
+            }
+
+            public void onPageFinished(WebView view, String url) {
+                Toast.makeText(activity, url, Toast.LENGTH_SHORT).show();
+            }
+        });
+        webview.loadUrl("https://www.google.nl/maps/place/Nederland/@52.191735,3.0369282,7z/data=!3m1!4b1!4m2!3m1!1s0x47c609c3db87e4bb:0xb3a175ceffbd0a9f");
+
     }
 
 }
