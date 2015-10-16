a8a2ae5cbb1d5c752e3bf8999034fdfe3c0a5260
Kilydan <tim.hoenselaar@gmail.com>
2015-10-16 12:39:41 +0200

    dfsgfds

--- app/src/main/java/com/internet/internet/Maps.java
+++ app/src/main/java/com/internet/internet/Maps.java
@@ -35,20 +35,6 @@ public class Maps extends AppCompatActivity {
         setContentView(webview);
         webview.getSettings().setJavaScriptEnabled(true);
         final Activity activity = this;
-        webview.setWebChromeClient(new WebChromeClient() {
-            public void onProgressChanged(WebView view, int progress) {
-                Toast.makeText(activity, Integer.toString(progress) + "%", Toast.LENGTH_SHORT).show();
-            }
-        });
-        webview.setWebViewClient(new WebViewClient() {
-            public void onReceivedError(WebView view, int errorCode, String description, String failingURL) {
-                Toast.makeText(activity, "Error!" + description, Toast.LENGTH_SHORT).show();
-            }
-
-            public void onPageFinished(WebView view, String url) {
-                Toast.makeText(activity, url, Toast.LENGTH_SHORT).show();
-            }
-        });
         webview.loadUrl("https://www.google.nl/maps/place/Nederland/@52.191735,3.0369282,7z/data=!3m1!4b1!4m2!3m1!1s0x47c609c3db87e4bb:0xb3a175ceffbd0a9f");
 
     }
