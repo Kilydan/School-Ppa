613ed7fc9f6a1c7e23669e481e6b352408f48989
Kilydan <tim.hoenselaar@gmail.com>
2015-10-19 11:56:33 +0200

    jose

--- app/src/main/java/com/internet/internet/Gmail.java
+++ app/src/main/java/com/internet/internet/Gmail.java
@@ -20,16 +20,6 @@ public class Gmail extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-
-        });
-
         final WebView webview = new WebView(this);
         setContentView(webview);
         webview.getSettings().setJavaScriptEnabled(true);
